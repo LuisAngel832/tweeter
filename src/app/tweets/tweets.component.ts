0c2c2b82c1ff90d939a86fb25edead8118abce79
Luis <luisgelipe.12340@gmail.com>
2025-05-22 21:55:50 -0600

    creacion del proyecto

---
+++ src/app/tweets/tweets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tweets',
+  standalone: false,
+  templateUrl: './tweets.component.html',
+  styleUrl: './tweets.component.css'
+})
+export class TweetsComponent {
+
+}
